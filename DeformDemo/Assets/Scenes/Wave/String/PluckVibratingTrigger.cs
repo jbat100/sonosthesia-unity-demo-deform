2bdb310b18ee52787e02d0399ce7c77e51039d3a
jonathan-opuscope <jonathan@opuscope.com>
2024-02-27 11:26:33 +0100

    Vibrating string test scene

---
+++ DeformDemo/Assets/Scenes/Wave/String/PluckVibratingTrigger.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Sonosthesia.Generator;
+using Sonosthesia.Trigger;
+using UnityEngine;
+
+namespace Sonosthesia
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+
+    [CustomEditor(typeof(PluckVibratingTrigger))]
+    public class PluckVibratingTriggerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            PluckVibratingTrigger trigger = (PluckVibratingTrigger)target;
+            if(GUILayout.Button("Trigger"))
+            {
+                trigger.Trigger();
+            }
+        }
+    }
+#endif
+    
+    public class PluckVibratingTrigger : MonoBehaviour
+    {
+        [SerializeField] private bool _resetTime;
+        
+        [SerializeField] private float _valueScale = 1f;
+        
+        [SerializeField] private float _timeScale = 1f;
+        
+        [SerializeField] private List<GeneratorSignal<float>> _generators;
+        
+        [SerializeField] private List<Triggerable> _triggerables;
+        
+        public void Trigger()
+        {
+            if (_resetTime)
+            {
+                foreach (GeneratorSignal<float> generator in _generators)
+                {
+                    generator.ResetTime();
+                }    
+            }
+
+            foreach (Triggerable triggerable in _triggerables)
+            {
+                triggerable.Trigger(_valueScale, _timeScale);
+            }
+        }
+    }    
+}
+
+
