87cbce581039b392a7ab1c25ff7ac069c2535e6f
jbat100 <johnny.frenchy@gmail.com>
2024-06-27 12:55:50 +0200

    Starting on signals to control polar spin

---
+++ DeformDemo/Assets/Scenes/Compositions/Polar/PolarAnimationProxy.cs
@@ -0,0 +1,11 @@
+using Sonosthesia.Timeline;
+
+namespace Sonosthesia
+{
+    public class PolarAnimationProxy : AnimationProxy
+    {
+        
+    }
+
+}
+
---
+++ DeformDemo/Assets/Scenes/Compositions/Polar/PolarSignalProxy.cs
@@ -0,0 +1,18 @@
+using Sonosthesia.Trajectory;
+using UnityEngine;
+
+namespace Sonosthesia
+{
+    public class PolarSignalProxy : MonoBehaviour
+    {
+        [SerializeField] private FloatTrajectory _dotsAngleTrajectory;
+        [SerializeField] private FloatTrajectory _linesAngleTrajectory;
+
+        public void Rest()
+        {
+            
+        }
+    }    
+}
+
+
