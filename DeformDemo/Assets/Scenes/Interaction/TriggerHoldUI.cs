0bc740da60fafbf4206dfd2a82982ac14a657d00
jbat100 <johnny.frenchy@gmail.com>
2024-10-11 11:12:12 +0200

    Use material package for shaders and realign

--- DeformDemo/Assets/Scenes/Interaction/TriggerHoldUI.cs
+++ DeformDemo/Assets/Scenes/Interaction/TriggerHoldUI.cs
@@ -9,8 +9,8 @@ namespace Sonosthesia
     {
         [SerializeField] private Slider _valueScaleSlider;
         
-        [SerializeField] private Triggerable _trigger;
-        [SerializeField] private TrackedTriggerable _hold;
+        [SerializeField] private Trigger.Trigger _trigger;
+        [SerializeField] private TrackedTrigger _hold;
 
         private Guid _holdId;
         
@@ -22,7 +22,7 @@ namespace Sonosthesia
                 return;
             }
             float valueScale = _valueScaleSlider ? _valueScaleSlider.value : 1f;
-            _trigger.Trigger(valueScale, 1f);
+            _trigger.StartTrigger(valueScale, 1f);
         }
 
         public void StartHold()
